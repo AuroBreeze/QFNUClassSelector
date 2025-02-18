37e74a6cae0c44aa2d3c69f32b4fe0499d058e66
AuroBreeze <1732373074@qq.com>
2025-02-18 10:46:30 +0800

    refactor(Select_class): 优化课程选择类的请求方式
    - 将 Fixed_Data 的 output 参数从 "Frame" 改为 "Params"，以获取参数数据
    - 在请求 URL 时使用 params 参数传递，以替代拼接 URL 字符串
    - 在 POST 请求中添加 params 参数，以确保所有必要数据都被发送
    - 移除了不必要的 print 语句

--- Module/Select_class.py
+++ Module/Select_class.py
@@ -1,13 +1,12 @@
 class Select_Class:
     def __init__(self,session,Fixed_Data,kcxx="",skls="",skxq="",skjc="",sfym="false",sfct="false",sfxx="true",skxq_xx0103=""):
-        self.frame = Fixed_Data(output="Frame",kcxx=kcxx, skls=skls, skxq=skxq, skjc=skjc, sfym=sfym, sfct=sfct, sfxx=sfxx, skxq_xx0103=skxq_xx0103).Return_Data()
+        self.params = Fixed_Data(output="Params",kcxx=kcxx, skls=skls, skxq=skxq, skjc=skjc, sfym=sfym, sfct=sfct, sfxx=sfxx, skxq_xx0103=skxq_xx0103).Return_Data()
         self.data = Fixed_Data(output="Data").Return_Data()
         self.session = session
-        self.url = "http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/xsxkGgxxkxk?" + self.frame
-        print(self.url)
+        self.url = "http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/xsxkGgxxkxk"
 
     def main(self):
-        res = self.session.get(url=self.url).text
-        res = self.session.post(url=self.url,data=self.data).json()
+        res = self.session.get(url=self.url,params=self.params).text
+        res = self.session.post(url=self.url,params=self.params,data=self.data).json()
         print(res)
 

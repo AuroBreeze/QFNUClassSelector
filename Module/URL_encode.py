069664247b9ba6da743d4bb2a73e291e0afd4ad5
AuroBreeze <1732373074@qq.com>
2025-02-18 10:45:52 +0800

    refactor(Module): 优化 URL编码功能
    - 修改了 Encode 类的 __init__ 方法，移除了默认参数
    - 更新了 Logging 模块的使用方式，传入了 "URL" 作为日志标识
    - 优化了代码结构，提高了可读性和可维护性

--- Module/URL_encode.py
+++ Module/URL_encode.py
@@ -2,18 +2,18 @@ import urllib.parse #实现URL编码
 from Module import Logging
 
 class Encode:
-    def __init__(self, text=""):
+    def __init__(self, text):
         "%E4%B9%92%E4%B9%93%E7%90%83"
         try:
             if text == "":
-                Logging.Log().main("WARN", "URL_encode.py 没有输入需要编码的文本,请重新输入")
+                Logging.Log("URL").main("WARN", "URL_encode.py 没有输入需要编码的文本,请重新输入")
             # 需要编码的字符串
             self.text = f"{text}"
             # URL 编码
             self.encoded_text = urllib.parse.quote(self.text)
             # print(self.encoded_text)
         except:
-            Logging.Log().main("ERROR", "URL_encode.py URL编码出现问题")
+            Logging.Log("URL").main("ERROR", "URL编码出现问题")
     def Get_encode(self):
         return self.encoded_text
 

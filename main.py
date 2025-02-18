b58600cf9032b0603f321ccf248489f3c460cbef
AuroBreeze <1732373074@qq.com>
2025-02-18 10:46:47 +0800

    feat: 添加 QFNU 选课器功能
    - 新增 QFNUClassSelector 类实现选课功能
    - 添加欢迎界面和登录

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from Module import URL_encode, login
+from Module import URL_encode, login,Logging,Welcome,Session_inherit,Select_class,Submit_class,Data
 
+class QFNUClassSelector:
+    def __init__(self):
+        pass
+
+    def run(self):
+        Welcome.main()
+        #Logging.Log().main("info", "QFNUClassSelector started")
+        session = login.mainss()
+        session  = Session_inherit.Session_Inherit(session).Return_Session()
+        Select_class.Select_Class(session,Data.Fixed_Data,kcxx=f"{URL_encode.Encode("乒乓球")}")
+        Submit_class.Submit_ClassSelection(session,jx0404id="202420252011613",kcid="530128")
 if __name__ == '__main__':
-    pass
+    QFNUClassSelector().run()
 

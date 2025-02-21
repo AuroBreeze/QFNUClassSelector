f9693f027f23f887f40ba1a676e9dafa89c6cf39
AuroBreeze <1732373074@qq.com>
2025-02-21 23:22:28 +0800

    refactor(main): 优化 QFNUClassSelector 类的 run 方法- 修改 login.mainss() 函数调用，添加参数 0- 在 QFNUClassSelector 类中添加 MultiAccount 方法占位符 - 优化代码格式和结构

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from Module import URL_encode, login,Logging,Welcome,Session_inherit,Select_clas
 
 class QFNUClassSelector:
     def __init__(self):
-
         pass
 
     def run(self):
         Welcome.main()
         bool_config = Check_toml.main().Return_bool()
-
         if bool_config == False:
             return
         Logging.Log().main("INFO", "QFNUClassSelector started")
 
-        session = login.mainss()
+        session = login.mainss(0)
         session  = Session_inherit.Session_Inherit(session).Return_Session()
         Select_class.Select_Class(session,Data.Fixed_Data,kcxx=f"{URL_encode.Encode("乒乓球")}")
         Submit_class.Submit_ClassSelection(session,jx0404id="202420252011613",kcid="530128")
+    def MultiAccount(self):
+        pass
+
 if __name__ == '__main__':
     QFNUClassSelector().run()
 

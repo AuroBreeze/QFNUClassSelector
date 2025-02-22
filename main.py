648c927ac2c1c79014e6b1e57cf544e93ee977c1
AuroBreeze <1732373074@qq.com>
2025-02-22 10:04:40 +0800

    feat(main): 添加运行时间计时功能
    - 在 QFNUClassSelector.run 方法中添加了计时功能
    - 在程序开始和结束时记录时间，并计算总运行时间
    - 增加了 Timer 模块的导入和使用- 优化了程序的结构，提高了可读性

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from Module import URL_encode, login,Logging,Welcome,Session_inherit,Select_class,Submit_class,Data,Check_toml
+from Module import URL_encode,Logging,Welcome,Session_inherit,Select_class,Submit_class,Data,Check_toml,Timer
+import time
 
 class QFNUClassSelector:
     def __init__(self):
@@ -6,20 +7,33 @@ class QFNUClassSelector:
         pass
 
     def run(self):
+        time_start = time.time()
+
         Welcome.main()
         bool_config = Check_toml.main().Return_bool()
         if bool_config == False:
             return
         self.log.main("INFO", "QFNUClassSelector started")
 
+        if Timer.Timer().run() == False:
+            self.log.main("INFO", "Time is up, exiting...")
+            time_end = time.time()
+            self.log.main("INFO", f"Time used: {time_end - time_start}s")
+            return
         #session = login.mainss(0)
         index = 0
         session  = Session_inherit.Session_Inherit(index).Return_Session()
         Select_class.Select_Class(session,Data.Fixed_Data,kcxx=f"{URL_encode.Encode("乒乓球")}")
         Submit_class.Submit_ClassSelection(session,jx0404id="202420252011613",kcid="530128")
+
+        time_end = time.time()
+        self.log.main("INFO", f"Time used: {time_end - time_start}s")
     def MultiAccount(self):
         pass
 
 if __name__ == '__main__':
+
     QFNUClassSelector().run()
+
+    #print("Time used:", time_end - time_start)
 

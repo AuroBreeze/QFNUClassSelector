dad3170a7843657891d59e1a7f7d7a84f9c2d542
AuroBreeze <1732373074@qq.com>
2025-02-22 22:15:35 +0800

    refactor(Module/Data.py): 重构 Fixed_Data 类
    - 添加了日志功能，通过引入 Logging 模块记录错误信息
    -增加了配置文件加载功能，使用 toml 模块解析配置
    - 优化了类的初始化方法，提高了代码的可读性和可维护性
    - 改进了 Return_Data 方法，增加了参数有效性检查
    - 移除了未使用的 URL 注释，简化了代码结构

--- Module/Data.py
+++ Module/Data.py
@@ -1,20 +1,41 @@
+from Module import Logging
+import toml
+
 class Fixed_Data:
-    # 固定提交的表单
-    # sEcho=1&iColumns=12&sColumns=&iDisplayStart=0&iDisplayLength=15&mDataProp_0=kch&mDataProp_1=kcmc&mDataProp_2=fzmc&mDataProp_3=xf&mDataProp_4=skls&mDataProp_5=sksj&mDataProp_6=skdd&mDataProp_7=xqmc&mDataProp_8=xkrs&mDataProp_9=syrs&mDataProp_10=ctsm&mDataProp_11=czOper
-    def __init__(self,output,kcxx="",skls="",skxq="",skjc="",sfym="false",sfct="false",sfxx="true",skxq_xx0103=""):
+    def __init__(self, output, kcxx, skls, skxq, skjc, sfym, sfct, sfxx, skxq_xx0103):
+        self.log = Logging.Log("Data")
 
         self.output = output # 返回固定参数
+
+        self.kcxx = kcxx #课程号
+        self.skls = skls #老师
+        self.skxq = skxq #星期
+        self.skjc = skjc #节次
+        self.sfym = sfym #是否过滤已满的课程 默认true
+        self.sfct = sfct #是否过滤冲突的课程 默认true
+        self.sfxx = sfxx #是否过滤限选的课程 默认true
+        self.skxq_xx0103 = skxq_xx0103 #1:曲阜 2：日照 68FD936EFC564F6E88EC852F9E8019C2：曲阜西校区
+
+        self.count_Te_1 = 0 # 教师列表index
+        self.count_Te_2 = 0 # 教师子列表index
+        self.count_Courses = 0 # 课程数量
+        self.time_Pr_1 = 0 # 时间列表index
+        self.time_Pr_2 = 0 # 时间子列表index
+        self.Wd_1 = 0 # 星期列表index
+        self.Wd_2 = 0 # 星期子列表index
+
+
         self.parmas = {
-            "kcxx": kcxx,# kcxx ：课程(URL编码)
-            "skls": skls,# skls 老师(URL编码)
-            "skxq": skxq,# skxq 星期 1 2 3 4 5 6 7 可为空
-            "skjc": skjc,# skjc 节次 1-2- 3-4- 5-6- 7-8- 9-10-11 12-13- 可为空
-            "sfym": sfym,# sfym 是否过滤已满的课程 默认false
-            "sfct": sfct,# sfct 是否过滤冲突的课程 默认false
-            "sfxx": sfxx,# sfxx 是否过滤限选的课程 默认true
-            "skxq_xx0103": skxq_xx0103# skxq_xx0103 1:曲阜 2：日照 68FD936EFC564F6E88EC852F9E8019C2：曲阜西校区
+            "kcxx": self.kcxx,
+            "skls": self.skls,
+            "skxq": self.skxq,
+            "skjc": self.skjc,
+            "sfym": self.sfym,
+            "sfct": self.sfct,
+            "sfxx": self.sfxx,
+            "skxq_xx0103": skxq_xx0103
         }
-        self.data_str = { #POST表单,查询课程表需要的参数
+        self.data_str = {
             "sEcho": "1",
             "iColumns": "12",
             "sColumns": "",
@@ -31,25 +52,28 @@ class Fixed_Data:
             "mDataProp_8": "xkrs",
             "mDataProp_9": "syrs",
             "mDataProp_10": "ctsm",
-            "mDataProp_11": "czOper"}
+            "mDataProp_11": "czOper"
+        }
 
+        self.config = None
 
-    def Return_Data(self): #拼接查询需要的参数
+    def Load_Config(self, config_file):  # 加载配置文件
+        try:
+            with open(config_file, "r", encoding="utf-8") as f:
+                self.config = toml.load(f)
+        except FileNotFoundError:
+            self.log.main("ERROR", "Config file not found")
+        except Exception as e:
+            self.log.main("ERROR", "Failed to load config file: " + str(e))
+
+    def Return_Data(self):  # 拼接查询需要的参数
         if self.output == None:
             return
-        if self.output == "Data":
+        elif self.output == "Data":
             return self.data_str
-        if self.output == "Params":
+        elif self.output == "Params":
             return self.parmas
-
-
-# 选课学分情况 http://zhjw.qfnu.edu.cn/jsxsd/xsxk/xsxk_tzsm
-# 必修选课 http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/comeInBxxk
-# 选修选课 http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/comeInXxxk 查询请求：http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/xsxkXxxk?
-# 本学期计划选课.json http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/comeInBxqjhxk 查询请求：http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/xsxkBxqjhxk?
-# 专业内跨年级选课 http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/comeInKnjxk  查询请求：http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/xsxkKnjxk?
-# 计划外选课 http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/comeInFawxk 查询请求：http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/xsxkFawxk?
-# 公选课选课 http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/comeInGgxxkxk 查询请求：http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/xsxkGgxxkxk?
-# 辅修选课 http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/comeInFxzyxk 查询请求：http://zhjw.qfnu.edu.cn/jsxsd/xsxkkc/xsxkFxxk?
+        else:
+            self.log.main("ERROR", "Return_Data: 无效的请求参数")
 
 # 退选API：http://zhjw.qfnu.edu.cn/jsxsd/xsxkjg/xstkOper?jx0404id=202420252011613&tkyy=&_=1739945760325 退课请求参数：jx0404id 课程号，tkyy 退课原因，_ 时间戳

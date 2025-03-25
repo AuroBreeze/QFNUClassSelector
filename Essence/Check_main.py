ca64a153db543d9d626c1600e19fed5bcbe5dd1d
AuroBreeze <1732373074@qq.com>
2025-03-25 11:49:17 +0800

    feat: 添加Check_main.py用于配置文件检查和日志记录
    添加Check_main.py文件，用于检查config.toml配置文件的有效性，并根据检查结果记录日志。如果配置文件检查通过，则生成JSON文件；否则，记录错误日志并退出程序。

---
+++ Essence/Check_main.py
@@ -0,0 +1,24 @@
+from Module import Check_toml,Welcome,Logging,Params_constructor
+import time
+class main:
+    def __init__(self):
+        time_1 = time.time()
+        self.log = Logging.Log("Check_Main")
+        Welcome.main()
+        #time.sleep(0.1)
+        judge = Check_toml.main().Return_bool()
+        #print(judge)
+        if judge:
+            self.log.main("INFO", "配置文件检查通过")
+            Params_constructor.ParamsConstructor().write_to_json()
+            time_2 = time.time()
+            self.log.main("INFO", "检查用时："+str(time_2-time_1)+"秒")
+        else:
+            time_2 = time.time()
+            self.log.main("ERROR", "config.toml配置文件检查未通过")
+            self.log.main("INFO", "用时："+str(time_2-time_1)+"秒")
+            self.log.main("ERROR", "按任意键退出...")
+            input()
+            exit()
+if __name__ == '__main__':
+    main()

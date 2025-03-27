b8ae4a32b2bb8b5b5b1bbaeff988e77f7339c467
AuroBreeze <1732373074@qq.com>
2025-03-27 11:53:58 +0000

    build: 添加 setup.py 文件以支持包的安装和分发
    - 创建 setup.py 文件，定义包的基本信息和依赖项
    - 包名设定为 QFNUCLASSSELECTOR，版本号为 1.4.2
    - 包含 log、json、WebUI、Module 和 Essence 目录下的模块
    - 当前未定义外部依赖项

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="QFNUCLASSSELECTOR",
+    version="1.4.2",
+    packages=find_packages(include=['log', 'json', 'WebUI', 'Module', 'Essence']),
+    install_requires=[]
+)

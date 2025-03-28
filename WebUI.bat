dc6abbc0af87b7af421c58b857b8fe1947a68b87
AuroBreeze <1732373074@qq.com>
2025-03-28 11:39:49 +0800

    build: 添加 Windows 环境下创建虚拟环境和运行 WebUI 的批处理脚本
    - 新增 creat_venv.bat脚本用于创建 Python 虚拟环境
    - 新增 WebUI.bat 脚本用于激活虚拟环境并运行 WebUI 脚本

---
+++ WebUI.bat
@@ -0,0 +1,8 @@
+:: 创建 Python 虚拟环境（假设虚拟环境目录为 .venv）
+python -m venv .venv
+
+:: 激活 Python 虚拟环境
+call .venv\Scripts\activate
+
+:: 运行 Python 脚本
+python WebUI\WebUI.py
---
+++ creat_venv.bat
@@ -0,0 +1,2 @@
+:: 创建 Python 虚拟环境（假设虚拟环境目录为 .venv）
+python -m venv .venv

beed13138497f4cb14144f50a23915cd8ba82626
AuroBreeze <1732373074@qq.com>
2025-03-28 11:47:55 +0800

    build: 更新 creat_venv.bat 脚本
    - 移除了创建虚拟环境时的注释
    - 在安装完 requirements.txt后，增加了安装当前项目的步骤

--- creat_venv.bat
+++ creat_venv.bat
@@ -1,7 +1,7 @@
-:: 创建 Python 虚拟环境（假设虚拟环境目录为 .venv）
 echo "创建 Python 虚拟环境"
 python -m venv .venv
 echo "激活 Python 虚拟环境"
 call .venv\Scripts\activate
 echo "安装依赖"
 pip install -r requirements.txt
+pip install -e .
